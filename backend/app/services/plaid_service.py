7bc133e12a3a3f0c787de839b6828ab237fa3304
KennethCodepuz <kennethseancorpuz14@gmail.com>
2026-07-21 21:09:41 +0700

    add: Finished implementing Link Token Creation and Public Token Exchange

--- backend/app/services/plaid_service.py
+++ backend/app/services/plaid_service.py
@@ -1,5 +1,10 @@
 import plaid
 from plaid.api import plaid_api
+from plaid.model.link_token_create_request import LinkTokenCreateRequest
+from plaid.model.link_token_create_request_user import LinkTokenCreateRequestUser
+from plaid.model.products import Products
+from plaid.model.country_code import CountryCode
+from plaid.model.item_public_token_exchange_request import ItemPublicTokenExchangeRequest
 from app.core.config import settings
 
 
@@ -21,3 +26,29 @@ def get_plaid_client() -> plaid_api.PlaidApi:
 
     api_client = plaid.ApiClient(configuration)
     return plaid_api.PlaidApi(api_client)
+
+def create_link_token(user_id: str):
+   client = get_plaid_client()
+   request = LinkTokenCreateRequest(
+     user=LinkTokenCreateRequestUser(
+       client_user_id=user_id
+     ),
+     products=[Products("auth"), Products("transactions")],
+     client_name="Finance Autopilot",
+     language="en",
+     country_codes=[CountryCode("US")],
+   )
+
+   response = client.link_token_create(request)
+   return response.to_dict()
+
+
+
+def exchange_public_token(public_token: str):
+   client = get_plaid_client()
+   request = ItemPublicTokenExchangeRequest(
+      public_token=public_token
+   )
+
+   response = client.item_public_token_exchange(request)
+   return response.to_dict()
